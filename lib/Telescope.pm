c1e12f744b9a641ea829f8898e18c88934fd504d
hiroyuki <hiroyukimm@gmail.com>
2012-03-17 22:30:05 +0900

    init

---
+++ lib/Telescope.pm
@@ -0,0 +1,32 @@
+package Telescope;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Telescope -
+
+=head1 SYNOPSIS
+
+  use Telescope;
+
+=head1 DESCRIPTION
+
+Telescope is
+
+=head1 AUTHOR
+
+yamanaka hiroyuki E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
